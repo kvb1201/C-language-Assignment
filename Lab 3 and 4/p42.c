e24b5b1b3e123a4e4305df541334c1d7c35816fe
u24ai040 <u24ai040@aid.svnit.ac.in>
2024-09-11 21:37:13 +0530

    Program to check if Number is Prime or Composite

---
+++ Lab 3 and 4/p42.c
@@ -0,0 +1,36 @@
+/* Program to check whether a number is Prime or Composite.*/
+//A Prime Number is a number that can only be divided by itself and 1 witout remainders.
+
+#include<stdio.h>
+int main()
+{
+    int number,i,reminder;
+    printf("Enter the number");
+    scanf("%d",&number);
+     
+    if(number<=1)
+    {
+        printf("%d is not a Prime number",number);// Numbers which are less then or equal to one are not a prime number.
+    }
+    else
+    {
+        for(i=2;i<=(number/2);++i)
+        {
+            if(number%i==0)
+            {
+            reminder=0;
+            break;
+            }
+        }
+        //reminder is 0 for all composite Number
+        if( reminder==0)
+        {
+            printf("%d is a  Composite number\n",number);
+        }
+        else
+        {
+            printf("%d is a Prime number\n",number);
+        }
+    }
+
+}
