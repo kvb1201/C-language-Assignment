d63f1c481d0e08b7351bf4d6c915f808b9e871c5
u24ai040 <u24ai040@aid.svnit.ac.in>
2024-09-25 18:36:23 +0530

    Program to read an Array of 10 Integers and Count total number of Odd and Even elements

---
+++ lab 5/p55.c
@@ -0,0 +1,42 @@
+// Program to read an Array of 10 Integers and Count total number of Odd and Even elements
+
+
+#include<stdio.h>
+int main()
+{
+    int a[10],sum=0,count=0,c2=0;
+    // Reading input from user
+    printf("Enter 10 numbers\n");
+     for(int i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+      
+    }
+    printf(" Matrix A is ");
+    // Printing Matrix A
+    for(int i=0;i<10;i++)
+    {
+        printf("%d\t",a[i]);   
+    }
+
+    for(int i=0;i<10;i++)
+    {
+        int flag=0;
+        flag=a[i]%2;//if Flag =0 ,then it is even number otherwise it is odd number 
+        if(flag==0)
+        {
+            count=count+1;// Counting Number of Even numbers
+        }
+        else
+        {
+            c2=c2+1;// Counting Number of Odd numbers
+        }
+    }
+    
+    printf("Total Number of Even Number in Array is %d\n",count);
+    printf("Total Number of Odd Number in Array is %d\n",c2);
+    }
+
+
+        
+    
