3c94492f76de74f18adbfe243f5b516250b0bc03
Vivek Patel <u24ai040@aid.svnit.ac.in>
2024-09-26 13:22:29 +0530

    Program to read array of N elements and Find out Maximum and Second Maximum numbers from elements of array

---
+++ lab 5/p57.c
@@ -0,0 +1,40 @@
+//Program to read array of N elements and Find out Maximum and Second Maximum numbers from elements of array
+
+#include<stdio.h>
+int main()
+{
+    int N=2,a[N],max,Secondmax;
+    printf("Enter numbers Of Elements in matrix\n");
+    scanf("%d",&N);
+
+    printf("Enter the %d Numbers\n",N);
+    // Reading input from user
+     for(int i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+      
+    }
+        printf("\nMatrix A is\n");
+     // Printing Matrix A
+    for(int i=0;i<N;i++)
+    {
+        printf("%d\t",a[i]);   
+    }
+    max=a[0];
+    Secondmax=max;
+
+
+     for(int i=0;i<N;i++)
+    {
+        if(a[i]>max)
+        {   Secondmax=max;
+            max=a[i];
+        }
+        else if (a[i]>Secondmax && a[i]<max)
+        {
+            Secondmax=a[i];
+        }
+            }
+            printf("\nMaximum Number is %d\n",max);
+            printf("Second Maximum Number is %d\n",Secondmax);
+}
