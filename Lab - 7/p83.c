d76d6287132694f71af780f86c2d860e4dd806b6
Vivek Patel <u24ai040@aid.svnit.ac.in>
2024-10-07 13:53:25 +0530

    Program to read an Array and find out maximum number among them

---
+++ Lab - 7/p83.c
@@ -0,0 +1,40 @@
+/*Program to read an Array and find out maximum number among them */
+#include<stdio.h>
+int maximum(int n,int a[])
+{
+    int Max;
+    Max=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>Max)
+        {
+            Max=a[i];
+        }
+    }
+   return Max;
+
+}
+
+int main()
+{
+    int n;
+    printf("Enter the size of array\n");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the %d number\n",n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Your array is\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    printf("\n");
+   int Max= maximum(n,a);
+    printf("Maximum number is %d\n",Max);
+    return 0;  
+
+
+}
