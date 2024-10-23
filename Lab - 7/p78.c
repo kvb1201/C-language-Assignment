dd60602324953e0c11b80009d27b1a5915943937
Vivek Patel <u24ai040@aid.svnit.ac.in>
2024-10-23 11:02:09 +0530

    Program to check whether number is Armstrong or not using function

--- Lab - 7/p78.c
+++ Lab - 7/p78.c
@@ -1,16 +1,26 @@
 /*Program to check whether number is Armstrong or not using function*/
 
 #include<stdio.h>
-void armstrong(int number)//Function Defination
+void Armstrong(int num);//Function Prototype
+
+int main()
 {
-    int nummber,sum=0,a,q;
-    a=number;
-    while(number>0)
-    {
-        
-        q=number%10;
+    int num;
+    printf("Enter the Number");
+    scanf("%d",&num);
+      Armstrong(num);//Calling Function
+ 
+}
+
+void Armstrong(int num)//Function Defination
+{
+   int sum=0,a,q;
+    a=num;
+    while(num>0)
+    {  
+        q=num%10;
         sum=q*q*q + sum;
-        number=number/10;
+        num=num/10;
     }
 
     if(a==sum)
@@ -22,16 +32,4 @@ void armstrong(int number)//Function Defination
         printf("%d is not Armstrong Number",a);
     }
 
-}
-
-    
- int main()
-{
-     int number;
-    
-
-    printf("Enter the number");
-    scanf("%d",&number);
-    armstrong(number);//Calling Function
-     return 0;
 }
