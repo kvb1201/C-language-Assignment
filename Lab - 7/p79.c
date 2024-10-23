c23518cdad08e8e47f473c8cb44ef958a6928ac4
Vivek Patel <u24ai040@aid.svnit.ac.in>
2024-10-23 11:02:52 +0530

    Program that interchange the value of two number using function

--- Lab - 7/p79.c
+++ Lab - 7/p79.c
@@ -1,21 +1,20 @@
 /* Program that interchange the value of two number using function*/
 #include<stdio.h>
-int swap(int x,int y)//Function Defination
-{
-    
-    int temp;
-    temp=y;
-    y=x;
-    x=temp;
+int swap(int A,int B);//Function Prototype
 
-    printf("x is %d\n",x);
-    printf("y is %d\n",y);
-}
 int main()
 {
-    int x,y;
-    printf("Enter the x and y\n");
-    scanf("%d%d",&x,&y);
-    swap(x,y);//Calling Function
-    return 0;
+    int A,B;
+    printf("Enter the A and B\n");
+    scanf("%d%d",&A,&B);
+ swap(A,B);//Calling Function
+ 
+}
+int swap(int A,int B)//Function Defination
+{
+    int temp;
+    temp=B;
+    B=A;
+    A=temp;
+    printf("A=%d and B=%d",A,B);
 }
