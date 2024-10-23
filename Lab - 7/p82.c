2d8bafd8ea53f9baf02190498f81747c9d072eae
Vivek Patel <u24ai040@aid.svnit.ac.in>
2024-10-23 11:08:49 +0530

    Program to make a function to calculate nCr

--- Lab - 7/p82.c
+++ Lab - 7/p82.c
@@ -1,43 +1,25 @@
 /*Program to make a function to calculate nCr.*/
 
 #include<stdio.h>
-long fact(int n)//Function Defination
-{
-    long int fact=1;
+int fact(int n);//Function Prototype
 
-    
-    for(int i=1;i<=n;i++)
-    {
-        fact=fact*i;
-    }
-    return fact;
-}
-long result(int n,int r)//Function Defination
-{
-   long int fresult;
-  
-    if(r>n)
-    {
-        
-        return 0;
-    }
-    
-    
-        fresult= fact(n)/(
-        fact(r)*fact(n-r));
-        return fresult;
-    
-}
 int main()
 {
-    int n,r;
-    long int fresult;
-   
-
-    printf("Enter the value of n and r");
+    int n,c,w,r;
+    printf("Enter the n and r\n ");
     scanf("%d%d",&n,&r);
-    
-   fresult=result(n,r);//Calling Function
-   printf("answer is %ld",fresult);
-return 0;
+ w=fact(n)/(fact(n-r)*fact(r));//Calling Function
+
+ printf("%dC%d = %d",n,r,w);
+
 }
+int fact(int n)//Function Defination
+{int x=1;
+    for(int i=1;i<=n;i++)
+    {
+        x=x*i;
+    }
+    return x;
+
+}
+    
