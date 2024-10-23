34e22406b22a87ec621e38a70e29430d3f0a94d6
Vivek Patel <u24ai040@aid.svnit.ac.in>
2024-10-23 10:59:49 +0530

    Program to check whether number is even or odd using function

--- Lab - 7/p76.c
+++ Lab - 7/p76.c
@@ -1,28 +1,24 @@
 /*Program to check whether number is even or odd using function*/
 
 #include<stdio.h>
-void even(int a)//Function Defination
+void even(int num);//Function Prototype
+
+int main()
 {
-    int n;
-    n=a%2;
-    if(n==0)
-    {
-        printf("%d is even number",a);
-
-    }
-    if(n==1)
-    {
-        printf("%d is odd number",a);
-    }
-
+    int num;
+    printf("Enter the Number");
+    scanf("%d",&num);
+ even(num);//Calling Function
 }
 
-    int main()
+void even(int num)//Function Defination
+{
+    if(num%2==0)
     {
-    int a;
-    printf("Enter the a");
-    scanf("%d",&a);
-
-    even(a);//Calling Function
-    return 0;
+        printf(" Number is even");
+    }
+    else
+    {
+        printf(" Number is odd");
+    }
 }
